8aa4c8bdd57f9cd403c92e74f1afc13d25124bdf
NgurahYuwanda <114911123+NgurahYuwanda@users.noreply.github.com>
2023-04-08 20:34:36 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -118,8 +118,8 @@ window.addEventListener('load', () => {
       .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
       .from(".idea-2", 0.7, ideaTextTrans)
       .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
-      .from(".idea-3", 0.7, ideaTextTrans)
-      .to(".idea-3 strong", 0.5, {
+      .from(".idea-3", 0.20, ideaTextTrans)
+      .to(".idea-3 strong", 0.7, {
         scale: 1.2,
         x: 10,
         backgroundColor: "rgb(21, 161, 237)",
